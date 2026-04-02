4c49cb4f35d37925140e7fa1b3f4e10bace68c17
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-04-02 15:20:05 +0200

    Ajout receiver_redis() fonctionnel #7

--- src/redis.py
+++ src/redis.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 import os
 
 def connect_redis():
+    """
+    Cree un HandShake entre Sentienl et Redis
+    success: OK
+    error: Erreur connexion Redis (int 1)
+    """
     load_dotenv(".env")
     try:
         r = redis.Redis(
@@ -17,3 +22,12 @@ def connect_redis():
     except Exception as e:
         print(f"\033[91mErreur connexion Redis: {e}\033[00m")
         exit(1)
+
+def receiver_redis(r: redis.Redis):
+    """
+    Recoit raw data du stream Redis (Liste)
+    """
+    while 1:
+        result = r.blpop(os.getenv("REDIS_KEY"), timeout=0)
+        _, raw = result
+        print(raw)
