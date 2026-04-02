c734c7dfb53c7b4a090f23d10f0599308506208b
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-04-02 15:19:35 +0200

    Ajout main.py appelle receiver_redis() et gere la cloture ctrl+c avec KeyboardInterrupt #7

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from src.env import check_for_environment
-from src.redis import connect_redis
+from src.redis import connect_redis, receiver_redis
 
 def main():
     """
@@ -10,6 +10,10 @@ def main():
     check_for_environment()
     ## TODO: add a return to check_for_environment() to handle in case of error
     r = connect_redis()
+    receiver_redis(r)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\033[91mArrêt du sentinel\033[00m")
