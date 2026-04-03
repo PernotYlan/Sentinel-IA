9b76ede0220f5d5243e7e4e7c1816444d8794b0f
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-04-03 15:41:39 +0200

    Update parse_syslog() recupere un dict et est fonctionnel

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,8 @@
 import json
 from src.db import dump_sqlite
 
+counters = {"zeek": 0, "syslog": 0, "db": 0}
+
 def parse_zeek(data: dict) -> dict:
     """
     fills a dic FRANCAIS OUBLIE PAS"""
@@ -20,20 +22,25 @@ def parse_zeek(data: dict) -> dict:
         "timestamp": data.get("@timestamp")
     }
 
-def parse_syslog():
-    print("nuh uh")
+def parse_syslog(data: dict):
+    dump_sqlite(data)
 
 def parsing_service_selector(raw: str):
     """
     ecrire qqch"""
     data = json.loads(raw)
     tags = data.get("tags", [])
+    print(f"TAGS: {tags}")
 
     if "zeek" in tags:
-        nothing = parse_zeek(data)
-        nothing = "\033[92mAll Good\033[00m"
-        print(nothing);
+        parse_zeek(data)
+        counters["zeek"] += 1
+        print(f"\033[92mAll Good Zeek [{counters['zeek']}]\033[00m")
     elif "beats_input_codec_plain_applied" in tags:
-        parse_syslog()
+        parse_syslog(data)
+        counters["syslog"] += 1
+        print(f"\033[94mAll Good Syslog [{counters['syslog']}]\033[00m")
     else:
         dump_sqlite(data)
+        counters["db"] += 1
+        print(f"\033[93mAll Good DB [{counters['db']}]\033[00m")
