fcd24499856905df2d98dc14159a09f625e11e27
CrackedOnTiti <thierry.bungaroo@epitech.eu>
2026-04-15 11:26:11 +0200

    Update main.py demarrage workers threadés N_WORKERS au lancement #18

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 from src.env import check_for_environment
 from src.redis import connect_redis, receiver_redis
 from src.db import init_db
+from src.worker import start_workers, stop_workers
+from src.logger import logger
 # from src.model_ae import init_ae
 # from src.model_if import init_if
+
+# // TODO: passer N_WORKERS a 4 quand le serveur avec 2 coeurs est disponible
+# // TODO: migrer vers multiprocessing quand ML sera reactivee (IF zeek_window reste Container 1, XGB+AE dans Container 2)
+N_WORKERS = 3
 
 def main():
     """
@@ -15,6 +21,7 @@ def main():
     init_db()
     # init_ae()
     # init_if()
+    start_workers(N_WORKERS)
     r = connect_redis()
     receiver_redis(r)
 
@@ -22,4 +29,5 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("\n\033[91mArrêt du sentinel\033[00m")
+        logger.info("Arrêt du sentinel")
+        stop_workers(N_WORKERS)
